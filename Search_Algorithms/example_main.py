e774de04b2617cd84f1f5b103de1dad62804cf74
mahdimahdieh <mmahdieh90@gmail.com>
2025-10-14 11:01:11 +0330

    implementing search project

---
+++ Search_Algorithms/example_main.py
@@ -0,0 +1,11 @@
+from env.env import play
+
+def bfs(state):
+    actions = []
+
+    # Implement Search algorithm here
+
+    return actions
+
+if __name__ == "__main__":
+    play('easy', bfs, delay=500)
